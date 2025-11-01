1ef0c32aba8a6acb58fd574ffb1ae3ceacea3547
japath59 <japath59@vt.edu>
2025-11-01 18:17:02 -0400

    Change to vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '',
-  assetPrefix: './',};
+    reactStrictMode: true,
+  };
 
 export default nextConfig;
